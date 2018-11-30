1cc4ebb58bf09c07ff0190db7006db1bdf5bd542
Urbenson Laurent <ulaurent12@gmail.com>
2018-11-30 00:36:34 +0000

    extract of bug

--- program.cs
+++ program.cs
@@ -27,12 +27,6 @@ namespace EbayScraperonsole
             Console.WriteLine("What do you want to search? \n");
             userValue = Console.ReadLine();
 
-            foreach (var word in userValue)
-            {
-                word+"+"
-            }
-
-
             var url = "https://www.ebay.com/sch/i.html?_nkw="+userValue+"&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=02908&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50";
 
             var httpClient = new HttpClient();
